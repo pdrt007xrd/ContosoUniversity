f33b540cd282846dd3e66b651ec8c6a3e9257e3a
P P <pedrito007xrd@gmail.com>
2022-05-04 12:40:58 -0400

    Agregar archivos de proyecto.

---
+++ Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Tittle { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿namespace ContosoUniversity.Models
+{
+
+       public enum Grade
+        {
+            A,B,C,D,f
+
+        }
+
+    public class Enrollment
+    {
+        public int EnnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        //-------------------------------\\ property navigation below
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+
+    }
+}
